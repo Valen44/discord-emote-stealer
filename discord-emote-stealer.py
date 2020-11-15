0f8e6f3baf71b5c2c8de34fa96b196a9f72663d4
Valen44 <74482687+Valen44@users.noreply.github.com>
2020-11-15 02:35:18 -0300

    Add files via upload

---
+++ discord-emote-stealer.py
@@ -0,0 +1,19 @@
+import re
+import requests
+import os
+from pathlib import Path
+
+with open("html.txt", "r", encoding="utf8") as f:
+    html = f.read()
+serverName = re.findall(r'(?<=img alt=").*?(?=")', html)
+links = re.findall(r"https://cdn.discordapp.com/emojis/\d+.png", html)
+filenames = re.findall(r'(?<=alt=":)\S*(?=:")', html)
+
+directory = "".join(serverName)
+
+Path("./{}".format(directory)).mkdir(parents=True, exist_ok=True)
+
+for name, link in zip(filenames, links):
+    r = requests.get(link)
+    with open(os.path.join("./{}".format(directory), name) + ".png", "wb") as f:
+        f.write(r.content)
